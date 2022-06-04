e1e74ad171d6b542af24c5f2ee10b9dbecd60ba0
trí nguyễn <tri.nguyen@base.vn>
2022-06-04 15:35:34 +0700

    ud

--- src/screens/Home/PracticeScreen/TimeStartPractice.tsx
+++ src/screens/Home/PracticeScreen/TimeStartPractice.tsx
@@ -1,6 +1,5 @@
 import React, { memo, useEffect, useState } from "react";
-import { Text } from "react-native";
-import moment from "moment";
+import { Stopwatch } from "react-native-stopwatch-timer";
 
 interface TimeStartPracticeProps {
   stopTime: number;
@@ -14,27 +13,20 @@ export const TimeStartPractice = memo(function TimeStartPractice({
   onReplay,
 }: TimeStartPracticeProps) {
   const [dt, setDt] = useState(0);
-  useEffect(() => {
-    if (stopTime == 0 || stopTime / 10000 >= dt) {
-      const secTimer = setInterval(async () => {
-        setDt(dt + 1000);
-      }, 1000);
-
-      return () => clearInterval(secTimer);
-    } else {
-      onReplay && onReplay(true);
-    }
-  }, [dt, stopTime, onReplay]);
 
   useEffect(() => {
     setDt(0);
   }, [replay, setDt]);
 
   return (
-    <Text>
-      {moment(dt || 0).format("mm:ss")} /{" "}
-      {moment(stopTime / 10000).format("mm:ss")}
-    </Text>
+    <Stopwatch
+      laps
+      msecs
+      start={true}
+      reset={false}
+      options={{}}
+      getTime={setDt}
+    />
   );
 });
 
